0249449e4d3250895f45d6333186a1a22a46307c
DESKTOP-PD4S3SD\Danny <dannymoss4@googlemail.com>
2020-01-07 14:24:23 +0000

    adds varibles file

---
+++ src/Styles/_variables.js
@@ -0,0 +1,15 @@
+const variables = {
+  colors: {
+    grey: '#222222',
+    blue: '#65abe9',
+    green: '#4bd4b0',
+    orange: '#ff6633'
+  },
+  fontSizes: {
+    xs: '8px',
+    s: '12px',
+    m: '18px',
+    l: '22px',
+    xl: '30px'
+  }
+}
