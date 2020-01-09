90be626f9a66b064b061992e176ea23c09928552
DESKTOP-PD4S3SD\Danny <dannymoss4@googlemail.com>
2020-01-09 10:37:46 +0000

    Updates shoe item and filter buttons

--- src/Components/ShoeItem/ShoeItem.js
+++ src/Components/ShoeItem/ShoeItem.js
@@ -7,10 +7,10 @@ function ShoeItem ({ name, category, size, colour, intials, status, image }) {
       <div className={`status-container bg-${status}`}>
         <span />
       </div>
+      <div className='shoe-img-container'>
+        <img src={image} alt={`shoe-img-${name}`} />
+      </div>
       <div className='main-container'>
-        <div className='shoe-img-container'>
-          <img src={image} alt={`shoe-img-${name}`} />
-        </div>
         <p className='name-container'>{name}</p>
         <div className='category-container'>
           <span>Category:</span>
